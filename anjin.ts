6857993e4630b8c0c0eea80d607b6461b6839697
Craig Leinoff <craig.leinoff@gmail.com>
2015-12-24 01:31:19 -0500

    Adding custom AnjinModule for my own stuff.

--- anjin.ts
+++ anjin.ts
@@ -14,6 +14,95 @@ class ObjectEntity {
     y: number;
 }
 
+module AnjinModule {
+    export class anjinStar {
+        static currX: number = null;
+        static currY: number = null;
+        static destX: number = null;
+        static destY: number = null;
+        static nextMove: string = "STOP";
+        static easyStar: easystar.js;
+    }
+    export interface ActorInterface {
+        name: string;
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+        sprite: Phaser.Sprite;
+    }
+    export class Keys {
+        up: Phaser.Key;
+        down: Phaser.Key;
+        left: Phaser.Key;
+        right: Phaser.Key;
+    }
+    class Actor implements ActorInterface {
+        private _name: string;
+        private _x: number;
+        private _y: number;
+        private _width: number = 64;
+        private _height: number = 64;
+        private _sprite: Phaser.Sprite;
+
+        get name():string {
+            return this._name;
+        }
+
+        set name(value:string) {
+            this._name = value;
+        }
+
+        get x():number {
+            return this._x;
+        }
+
+        set x(value:number) {
+            this._x = value;
+        }
+
+        get y():number {
+            return this._y;
+        }
+
+        set y(value:number) {
+            this._y = value;
+        }
+
+        get width():number {
+            return this._width;
+        }
+
+        set width(value:number) {
+            this._width = value;
+        }
+
+        get height():number {
+            return this._height;
+        }
+
+        set height(value:number) {
+            this._height = value;
+        }
+        get sprite(): Phaser.Sprite {
+            return this._sprite;
+        }
+        set sprite(newSprite: Phaser.Sprite) {
+            this._sprite = newSprite;
+        }
+    }
+
+    export class PlayerActor extends Actor implements ActorInterface {
+
+    }
+    export class NonPlayerActor extends Actor implements ActorInterface {
+
+    }
+}
+
+
+
+
 class Anjin {
 
     constructor() {
